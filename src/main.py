7ddcdd57efd8d78e2083678a7c39e3ac97d21718
XelaSnad <alexsandersofficial@outlook.com>
2022-07-13 12:02:01 +1000

    initial

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+import sys
+
+if len(sys.argv) != 3: 
+    print("Number of arguments incorrect, this program requires sample input and a binary.")
+
+binary = sys.argv[1] 
+binaryInput = sys.argv[2]
