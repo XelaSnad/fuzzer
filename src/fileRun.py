f6d4a4babd2396c031620d3b6eeb56d647984d84
XelaSnad <alexsandersofficial@outlook.com>
2022-07-16 21:55:01 +1000

    Started the CSV fuzzing

--- src/fileRun.py
+++ src/fileRun.py
@@ -4,15 +4,15 @@ import tempfile
 
 def fileExecuteText(program, text) :
     text = text.encode('utf-8')
-
+    
     p = subprocess.Popen([program], stdout = subprocess.PIPE, stdin = subprocess.PIPE)
     out = p.communicate(input=text)
-    return out[0].decode()
+    return (out, p, text)
 
 '''
 This function runs a list of inputs into a binary and returns a list of the resulting output
 '''
-def runTrials(binary, textList)
+def runTrials(binary, textList):
     returnList = []
     for idx, text in enumerate(textList):
         result = fileExecuteText(binary, text)
